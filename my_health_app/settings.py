5ec79cb4eb953d469e862180150597f6e07d3ed9
jcmontano13 <jeck.montano@yahoo.com>
2025-10-22 23:24:05 +1300

    configure to deploy to vercel. 1. update settings.py to explose static url 2. update vercel.json for deployment configuration

--- my_health_app/settings.py
+++ my_health_app/settings.py
@@ -89,8 +89,8 @@ USE_I18N = True
 USE_TZ = True
 
 # --- Static files ---
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # --- Default PK ---
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"

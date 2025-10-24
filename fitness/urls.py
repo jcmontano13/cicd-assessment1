98a291133dc71771acedff123328af2478bdbf6e
jcmontano13 <jeck.montano@yahoo.com>
2025-10-25 00:01:26 +1300

    create view individual entry functionality 1. update fitness/urls.py to include the path of the new function 2. update views.py to include the new function

--- fitness/urls.py
+++ fitness/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ActivityCreateView, ActivityListView
+from .views import ActivityCreateView, ActivityListView, ActivityDetailView
 
 urlpatterns = [
     path("activity/", ActivityCreateView.as_view(), name="activity-create"),
     path("activity/list/", ActivityListView.as_view(), name="activity-list"),
+    path("activity/<int:pk>/", ActivityDetailView.as_view(), name="activity-detail"),
 ]
